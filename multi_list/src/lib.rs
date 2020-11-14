1398c6cce3bb72e7b341712e15f9f1c3875a931e
MucTepDayH16 <denisdrozhzhin1999@gmail.com>
2020-11-14 23:54:41 +0300

    Added multi_list structure

---
+++ multi_list/src/lib.rs
@@ -0,0 +1,119 @@
+pub mod list;
+
+#[cfg(test)]
+mod list_test {
+    use crate::list::MultiList;
+
+    #[test]
+    fn push_pop() {
+        let mut master = MultiList::new();
+
+        master
+            .push(" heh!")
+            .push(" world!");
+
+        let mut branch = master.branch();
+
+        master
+            .push("Hello,");
+        branch
+            .push(" cruel")
+            .push("Goodbye,");
+
+        assert_eq!(branch.pop(), Some("Goodbye,"));
+        assert_eq!(branch.pop(), Some(" cruel"));
+        assert_ne!(branch.pop(), Some(" world!"));
+        assert_eq!(branch.pop(), None);
+
+        assert_eq!(master.pop(), Some("Hello,"));
+        assert_eq!(master.pop(), Some(" world!"));
+        assert_eq!(master.pop(), Some(" heh!"));
+        assert_eq!(master.pop(), None);
+    }
+
+    use std::{
+        ops::{Add,Mul},
+        cmp::max
+    };
+
+    #[test]
+    fn evaluate_str() {
+        let mut master = MultiList::new();
+        let mut branch;
+        master
+            .push("ld!")
+            .push("wor");
+        branch = master.branch();
+        branch
+            .push(" cruel")
+            .push("Goodbye,");
+        master
+            .push("o, ")
+            .push("Hell");
+
+        let std_add = |eval: String, item: &&str| eval + *item;
+        assert_eq!("Hello, world!", master.evaluate(
+            std_add,
+            "".to_string()
+        ));
+    }
+
+    #[test]
+    fn evaluate_i32() {
+        let array: Vec<i32> = (1..=4).collect();
+        let mut master = MultiList::new();
+        for i in array.iter() {
+            master.push(*i);
+        }
+
+        assert_eq!(
+            array.iter().sum::<i32>(),
+            master.evaluate(i32::add, 0)
+        );
+        assert_eq!(
+            array.iter().product::<i32>(),
+            master.evaluate(i32::mul, 1)
+        );
+        assert_eq!(
+            array.iter().max(),
+            Some(master.evaluate(max::<&i32>, &i32::MIN))
+        )
+    }
+
+    #[test]
+    fn sub_list() {
+        let mut master = MultiList::new();
+        master
+            .push("A")
+            .push("B")
+            .push("C")
+            .push("D");
+
+        let mut iter = MultiList::from_to(master.find(&"C"), master.find(&"A"));
+
+        assert_eq!(Some(&"C"), iter.next());
+        assert_eq!(Some(&"B"), iter.next());
+        assert_ne!(Some(&"A"), iter.next());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn common_node() {
+        let mut master = MultiList::new();
+        master
+            .push("A")
+            .push("B");
+
+        let mut branch = master.branch();
+        branch
+            .push("C");
+
+        master
+            .push("D")
+            .push("E");
+
+        let node_b = master.find(&"B");
+        assert_eq!(node_b, branch.find(&"B"));
+        assert_eq!(node_b, MultiList::common(&master, &branch));
+    }
+}
---
+++ multi_list/src/list.rs
@@ -0,0 +1,207 @@
+use std::{
+    rc::Rc,
+    mem::take,
+    ops::Deref,
+    fmt::{
+        Display,
+        Formatter,
+        Result
+    },
+    cmp::PartialEq
+};
+
+type Ref<T> = Option<Rc<T>>;
+
+#[derive(Debug,Default)]
+struct Node<T> {
+    item: T,
+    next: Ref<Node<T>>
+}
+
+impl<T> PartialEq for &Node<T> {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq((*self) as *const Node<T>, (*other) as *const Node<T>)
+    }
+}
+
+#[derive(Debug,Clone)]
+pub struct NodeIter<'a, T>(Option<&'a Node<T>>,Option<&'a Node<T>>);
+
+impl<'a, T> PartialEq for NodeIter<'a, T> {
+    fn eq(&self, other: &Self) -> bool {
+        match (self.0, other.0) {
+            (Some(node1), Some(node2)) => node1 == node2,
+            _ => false
+        }
+    }
+}
+
+impl<'a, T> Iterator for NodeIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(node) = self.0 {
+            if let Some(last_node) = self.1 {
+                if node == last_node {
+                    return None;
+                }
+            }
+            self.0 = match &node.next {
+                Some(rf) => Some(rf.deref()),
+                None => None
+            };
+            Some(&node.item)
+        } else {
+            None
+        }
+    }
+}
+
+/// Single linked list, that could be linked to  other list
+///
+/// # Example #
+///
+/// ```
+/// use multi_list::list::MultiList;
+/// use std::ops::{Add,Mul};
+///
+/// let mut list_str = MultiList::new();
+///
+/// list_str
+///     .push(" world!")
+///     .push("Hello,");
+///
+/// // [ head -> [1]Hello, -> [1] world! ]
+/// println!("{}", list_str);
+///
+/// let mut list_i32 = MultiList::new();
+///
+/// list_i32
+///     .push(1)
+///     .push(2)
+///     .push(3)
+///     .push(4);
+///
+/// // 10
+/// println!("{}", list_i32.evaluate(i32::add, 0));
+///
+/// // 24
+/// println!("{}", list_i32.evaluate(i32::mul, 1));
+/// ```
+///
+pub struct MultiList<T> {
+    head: Ref<Node<T>>
+}
+
+impl<T: Display> Display for MultiList<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let mut rf = &self.head;
+
+        write!(f, "[ head")?;
+        while let Some(node) = rf {
+            write!(f, " -> [{}]{}", Rc::strong_count(node), node.item)?;
+            rf = &node.next;
+        }
+        write!(f, " ]")
+    }
+}
+
+impl<T> MultiList<T> {
+    #[inline]
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
+    #[inline]
+    pub fn iter(&self) -> NodeIter<T> {
+        NodeIter(match &self.head {
+            Some(node) => Some(&node),
+            None => None
+        }, None)
+    }
+
+    #[inline]
+    pub fn iter_to<'a>(&'a self, node: NodeIter<'a, T>) -> NodeIter<'a, T> {
+        NodeIter(match &self.head {
+            Some(node) => Some(&node),
+            None => None
+        }, node.0)
+    }
+
+    #[inline]
+    pub fn from_to<'a>(first: NodeIter<'a, T>, last: NodeIter<'a, T>) -> NodeIter<'a, T> {
+        NodeIter(first.0, last.0)
+    }
+
+    #[inline]
+    pub fn branch(&self) -> Self {
+        Self {head: match &self.head {
+            Some(rf) => Some(rf.clone()),
+            None => None
+        }}
+    }
+
+    pub fn push(&mut self, item: T) -> &mut Self {
+        self.head = Some(Rc::new(Node {
+            item,
+            next: take(&mut self.head)
+        }));
+        self
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if let Some(rf) = take(&mut self.head) {
+            if let Ok(node) = Rc::try_unwrap(rf) {
+                self.head = node.next;
+                Some(node.item)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn evaluate<'a, U>(&'a self, fnc: fn(U, &'a T) -> U, mut def: U) -> U {
+        let mut rf = &self.head;
+        while let Some(node) = rf {
+            def = fnc( def, &node.item );
+            rf = &node.next;
+        }
+        def
+    }
+
+    pub fn common<'a>(&'a self, other: &'a Self) -> NodeIter<'a, T> {
+        let mut rf0 = &self.head;
+        while let Some(node0) = rf0 {
+            if Rc::strong_count(node0) > 1 {
+                let mut rf1 = &other.head;
+                while let Some(node1) = rf1 {
+                    if Rc::ptr_eq( node0, node1 ) {
+                        return NodeIter(Some(node0.deref()), None);
+                    }
+                    rf1 = &node1.next;
+                }
+            }
+            rf0 = &node0.next;
+        }
+        NodeIter(None, None)
+    }
+}
+
+impl<T: PartialEq> MultiList<T> {
+    pub fn find(&self, item: &T) -> NodeIter<T> {
+        let mut rf = &self.head;
+        while let Some(node) = rf {
+            if node.item.eq(item) {
+                return NodeIter(
+                    Some(node.deref()),
+                    None
+                );
+            } else {
+                rf = &node.next;
+            }
+        }
+        NodeIter(None, None)
+    }
+}
