8480210804e59b22688527d7b9dec6a61a24ce6f
muctep_dehuc <denis.drozhzhin@equilibrium.io>
2022-04-05 02:14:12 +0300

    Update future crate

--- future/src/main.rs
+++ future/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(non_camel_case_types, irrefutable_let_patterns)]
 
-use std::pin::Pin;
 use futures::{
     future::Future,
     task::{Context, Poll},
 };
+use std::pin::Pin;
 
 enum stage_add2 {
     Start,
@@ -26,7 +26,7 @@ impl fut_add2 {
 
 impl Future for fut_add2 {
     type Output = usize;
-    
+
     fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
         if let stage_add2::Start = self.stage {
             return Poll::Ready(self.args.0 + self.args.1);
@@ -37,8 +37,8 @@ impl Future for fut_add2 {
 
 enum stage_add3 {
     Start,
-    Stage0{ fut: fut_add2 },
-    Stage1{ fut: fut_add2 },
+    Stage0 { fut: fut_add2 },
+    Stage1 { fut: fut_add2 },
 }
 
 struct fut_add3 {
@@ -57,18 +57,18 @@ impl fut_add3 {
 
 impl Future for fut_add3 {
     type Output = usize;
-    
+
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         if let stage_add3::Start = self.stage {
             let fut = fut_add2::new(self.args.0, self.args.1);
-            self.stage = stage_add3::Stage0{ fut };
+            self.stage = stage_add3::Stage0 { fut };
         }
-        if let stage_add3::Stage0{ ref mut fut } = self.stage {
+        if let stage_add3::Stage0 { ref mut fut } = self.stage {
             let t = futures::ready!(Pin::new(fut).poll(cx));
             let fut = fut_add2::new(t, self.args.2);
-            self.stage = stage_add3::Stage1{ fut };
+            self.stage = stage_add3::Stage1 { fut };
         }
-        if let stage_add3::Stage1{ ref mut fut } = self.stage {
+        if let stage_add3::Stage1 { ref mut fut } = self.stage {
             let r = futures::ready!(Pin::new(fut).poll(cx));
             return Poll::Ready(r);
         }
