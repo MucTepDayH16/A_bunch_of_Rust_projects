93ea6539acbd9eea37bfb5da990caefd4689c1c7
MucTepDayH16 <denisdrozhzhin1999@gmail.com>
2023-06-06 12:57:41 +0300

    Update primes
    Signed-off-by: MucTepDayH16 <denisdrozhzhin1999@gmail.com>

--- primes/src/main.rs
+++ primes/src/main.rs
@@ -1,25 +1,20 @@
-#![feature(box_syntax)]
-
 use std::mem::take;
 
-type It<T> = Box<dyn Iterator<Item=T>>;
-
-struct Primes<T> (Option<It<T>>);
+struct Primes<T>(Option<Box<dyn Iterator<Item = T>>>);
 
 impl Iterator for Primes<u64> {
     type Item = u64;
-    
+
     fn next(&mut self) -> Option<u64> {
         let mut it = take(&mut self.0)?;
         let p = it.next()?;
-        self.0 = Some(box it.filter(move |x| x % p != 0));
+        self.0 = Some(Box::new(it.filter(move |x| x % p != 0)));
         Some(p)
     }
 }
 
-fn primes<I>(it: I) -> It<u64>
-where I: 'static + Iterator<Item=u64> {
-    box Primes(Some(box it))
+fn primes() -> impl Iterator<Item = u64> {
+    Primes(Some(Box::new(2..)))
 }
 
 fn read_stdin<T: std::str::FromStr>() -> Option<T> {
@@ -27,19 +22,22 @@ fn read_stdin<T: std::str::FromStr>() -> Option<T> {
     std::io::stdin()
         .read_line(&mut input)
         .expect("Failed to read from stdin");
-	
-	input.trim().parse().ok()
+
+    input.trim().parse().ok()
 }
 
 fn main() {
-	loop {
-		println!("Enter count of prime numbers:");
-		let x: usize = match read_stdin() {
-			Some(val) => val,
-			None => {println!("You have to enter valid number!"); continue},
-		};
-		
-		println!("First {} prime numbers:", x);
-		println!("{:?}", primes(2..).take(x).collect::<Vec<u64>>());
-	}
+    loop {
+        println!("Enter count of prime numbers:");
+        let x: usize = match read_stdin() {
+            Some(val) => val,
+            None => {
+                println!("You have to enter valid number!");
+                continue;
+            }
+        };
+
+        println!("First {} prime numbers:", x);
+        println!("{:?}", primes().take(x).collect::<Vec<u64>>());
+    }
 }
